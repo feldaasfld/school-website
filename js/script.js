644caa875b39ad999a7c28da5fc6a5638191735a
feldaasfld <feldaamelia25@gmail.com>
2023-03-04 16:42:01 +0700

    import js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    var shrinkHeader = 100;
+    $(window).scroll(function () {
+        var scroll = getCurrentScroll();
+        if (scroll >= shrinkHeader) {
+            $('nav').addClass('shrink');
+            $('#scrolltop').addClass('show-scroll');
+        } else {
+            $('nav').removeClass('shrink');
+            $('#scrolltop').removeClass('show-scroll');
+        }
+    });
+
+    function getCurrentScroll() {
+        return window.pageYOffset;
+    }
+});
